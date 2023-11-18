40c9d8b389a99ebd29f62bb4ad5757aacb5a1d1b
cabranca <joaquincabrera98@gmail.com>
2023-11-17 21:48:36 -0300

    Agrego escena de Basura y scripts para esta y Nivel_1

---
+++ Messi-Kitty_projecto/Codigo/nivel_1.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export var basura_scene: PackedScene
+var count
+
+func _ready():
+	new_game()
+	
+func _process(delta):
+	pass
+	
+func new_game():
+	count = 0
+	$BasuraTimer.start()
+
+func game_over():
+	$GarbageTimer.stop()
+
+# Cuando el timer llega a cero spawneo nueva basura
+# en una posicion random
+func _on_basura_timer_timeout():
+	# Creo la nueva instancia de la escena Basura
+	var basura = basura_scene.instantiate()
+	
+	# Genero una posicion random inicial (probar get_viewport_rect())
+	var xPos = randf() * get_viewport().get_visible_rect().size.x
+	var yPos = randf() * get_viewport().get_visible_rect().size.y
+	
+	# Seteo la posicion a la nueva basura
+	basura.position = Vector2(xPos, yPos)
+	
+	# Agrego el nuevo objeto como hijo del nodo principal
+	add_child(basura)
