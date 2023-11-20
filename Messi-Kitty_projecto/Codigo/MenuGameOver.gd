d73c9f4b180ac599e755f3a5610517138ed708cf
Micaela Gallinger <mica.gallinger@gmail.com>
2023-11-20 00:09:13 -0300

    Pantalla final
    Más fondo. Limpieza de algunos placeholders

--- Messi-Kitty_projecto/Codigo/MenuGameOver.gd
+++ Messi-Kitty_projecto/Codigo/MenuGameOver.gd
@@ -1,6 +1,8 @@
 extends Node
 
-
+func _ready():
+	$Melodia.play()
+	$Pantalla/AspectRatioContainer/VBoxContainer/HBoxContainer2/Numero.text = str(Variables.puntaje)
 func _on_boton_reiniciar_button_up():
 	get_tree().change_scene_to_file("res://Escenas/nivel_1.tscn")
 
