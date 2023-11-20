9fa31f1e02b88c1d1c88614c271a0ac799aa7acf
Micaela Gallinger <mica.gallinger@gmail.com>
2023-11-19 21:38:18 -0300

    Menú principal
    Funcionando

--- Messi-Kitty_projecto/MenuGameOver.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-
-func _on_boton_reiniciar_button_up():
-	get_tree().change_scene_to_file("res://Escenas/nivel_1.tscn")
-
-
-func _on_boton_salir_button_up():
-	get_tree().change_scene_to_file("res://Escenas/pantalla_principal.tscn")
